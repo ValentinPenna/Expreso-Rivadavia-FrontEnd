1a8ed505c6a5365ad4406e4f24c75c0d9f523ade
Valentin <valentinignaciopenna@gmail.com>
2024-06-26 11:46:48 -0300

    correcciones footer y cotizacion

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -18,7 +18,7 @@ export const Input: React.FC<InputProps> = ({
     <div className="flex flex-col">
       <label
         htmlFor={name}
-        className="text-[#CB1B1A] lg:text-4xl md:text-2xl sm:text-xl my-5"
+        className="text-primary lg:text-4xl md:text-2xl sm:text-xl my-5"
       >
         {label}
       </label>
@@ -27,12 +27,12 @@ export const Input: React.FC<InputProps> = ({
         id={name}
         name={name}
         placeholder={placeholder}
-        className="bg-transparent border-b border-[#CB1B1A] lg:text-4xl md:text-2xl sm:text-xl w-3/4"
+        className="bg-transparent border-b border-primary lg:text-3xl md:text-2xl sm:text-xl w-3/4"
       />
        <ErrorMessage
                     name={name}
                     component={() => (
-                      <div className="text-[#CB1B1A]">{error}</div>
+                      <div className="text-primary">{error}</div>
                     )}
                   />
     </div>
