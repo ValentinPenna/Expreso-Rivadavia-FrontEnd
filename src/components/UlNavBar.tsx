33a8c99241b30e93b3f4443a130998b9b47bf0e5
NicoDiaz <nicolas.diaz.bn@gmail.com>
2024-06-27 22:35:26 -0500

    navbar responsive

---
+++ src/components/UlNavBar.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import SwitchGuestUser from "./SwitchGuestUser";
+
+const UlNavBar = () => {
+  const [isFocused, setIsFocused] = useState<boolean>(true);
+  return (
+    <>
+      <button
+        onBlur={() => setIsFocused(false)}
+        className=" right-4 absolute z-50 flex flex-col gap-[3px] rounded-lg bg-secundary px-2 py-2 text-soft-letter lg:hidden"
+        onClick={() => setIsFocused(!isFocused)}
+      >
+        <CiMenuBurger className="text-xl" color="#CB1B1A" />
+      </button>
+      <ul
+        onMouseDown={(e) => e.preventDefault()}
+        className={`${isFocused ? "block gap-4 p-4 " : "hidden"} 
+        bg-white flex flex-col-reverse shadow-lg text-black text-lg font-normal duration-400 right-0 -bottom-64 absolute z-50 lg:static lg:shadow-none lg:flex lg:w-full lg:flex-row lg:items-center  lg:gap-3 lg:bg-transparent`}
+      >
+        <li>
+          <a href="/" className="hover:text-primary">
+            Cotizar un envío
+          </a>
+        </li>
+        <li>
+          <a href="/new-shipment" className="hover:text-primary">
+            Hacer un envío
+          </a>
+        </li>
+        <li>
+          <a href="/faqs" className="hover:text-primary">
+            Preguntas Frecuentes
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="hover:text-primary">
+            Sobre Nosotros
+          </a>
+        </li>
+        <SwitchGuestUser />
+      </ul>
+    </>
+  );
+};
+
+export default UlNavBar;
