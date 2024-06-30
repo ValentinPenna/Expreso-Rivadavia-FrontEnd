c2d923d29ca16950d825923b60dea5528f3fe615
Valentin <valentinignaciopenna@gmail.com>
2024-06-30 19:41:52 -0300

    correccion validacion login

--- src/components/validation/validateLogin.ts
+++ src/components/validation/validateLogin.ts
@@ -17,7 +17,7 @@ export const validateLogin = (values: ILogin): ILoginError => {
   ) {
     errors.email =
       "El correo solo puede contener letras, números, puntos, guiones y guion bajo";
-  }else if (values.email.length < 50) {
+  }else if (values.email.length > 50) {
     errors.email = "El correo no puede tener más de 50 caracteres";
   }   
   if (!values.password) {
