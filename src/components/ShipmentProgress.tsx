10315cc16f9a5dbb578ec40fb45f7d443d85e73a
fatimdaruich <fatimdaruich.fd@gmail.com>
2024-07-04 17:52:46 -0300

    algo

--- src/components/ShipmentProgress.tsx
+++ src/components/ShipmentProgress.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import ShipmentStatus from './ShipmentStatus';
 
 const ShipmentProgress = () => {
-  const [currentStatus, setCurrentStatus] = useState('');
+  
 
   return (
     <section className="w-full text-center my-4 bg-white px-12 py-16 rounded-l-lg flex flex-col max-w-[80%] gap-2">
-      <h1 className='text-xl font-bold text-primary'>Progreso del envío</h1>
+      <h1 className='text-2xl font-bold text-primary'>Progreso del envío</h1>
     <div className="flex justify-center mt-10">
-    <ShipmentStatus currentStatus={currentStatus} />
+    <ShipmentStatus />
    </div>
    </section>
   )
--- src/components/ShipmentStatus.tsx
+++ src/components/ShipmentStatus.tsx
@@ -1,28 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 const shipmentState = [
-  'El paquete salio de origen',
-  'El paquete se encuentra en tránsito',
-  'El paquete se encuentra llegando a destino',
-  'Paquete entregado',
+  'Recibido',
+  'En camino',
+  'Llegando a destino',
+  'Entregado  ✔',
 ];
 
 
 
-const ShipmentStatus = ({ currentStatus }: { currentStatus: string }) => {
-  const currentIndex = shipmentState.indexOf(currentStatus);
+const ShipmentStatus = () => {
+  
   
   return (
-   
-      <div className="relative pl-6">
-      
+      <div className="relative pl-6">  
       <div className="flex flex-col gap-10">
-        {shipmentState.map((status, index) => (
+        {shipmentState.map((status) => (
           <div key={status} className="flex items-center ">
             <div className='w-5 h-5 rounded-full bg-primary'></div>
-            
             <div className="ml-3 text-xl">{status}</div>
             
+
           </div>
         ))}
       </div>
