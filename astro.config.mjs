416bb9c078db6cf8c4dbbfbc07f3db36e244e2b8
NicoDiaz <nicolas.diaz.bn@gmail.com>
2024-07-09 09:02:26 -0500

    se agrego vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "hybrid",
   define: {
-    "import.meta.env.API_URL": JSON.stringify(process.env.API_URL),
-  }
+    "import.meta.env.API_URL": JSON.stringify(process.env.API_URL)
+  },
+  adapter: vercel()
 });
